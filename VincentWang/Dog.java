fbc2c91203798d6a7491dee253f8063810e1abf6
VinoLoser <vinowang921@gmail.com>
2020-06-24 17:04:01 -0400

    Commit to master

--- VincentWang/Dog.java
+++ VincentWang/Dog.java
@@ -12,13 +12,27 @@ public class Dog {
         this.age = age;
         rabies = false;
     }
-   
     public void bark() {
-        System.out.println("bork");
+        System.out.println("I dont want to bark");
     }
-    
+    public int getAge() {
+        return age;
+    }
+    public int getAgeInDogYears() {
+        return age/7;
+    }
+    public boolean hasRabies() {
+        return rabies;
+    }
+    public void setRabies(boolean gotRabies) {
+        rabies = gotRabies;
+    }
     public static void main(String[] args) {
-        Dog foo = new Dog("foo", "golden retriever", 5);
-        foo.bark();
+        Dog bar = new Dog("bar", "dachschung", 1);
+        bar.bark();
+        System.out.println(bar.getAge());
+        bar.setRabies(true);
+        System.out.println(bar.getAgeInDogYears());
+        System.out.println(bar.hasRabies());
     }
 }
